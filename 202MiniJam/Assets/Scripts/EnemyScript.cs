43629263627e0a124ab583d48e51635da2b6d7d8
Yonathan <yonathaneniyew2007@gmail.com>
2026-01-17 15:21:01 -0500

    Enemy stuff

---
+++ 202MiniJam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Attacks
+        int attackChoice = Random.Range(0, 3);
+
+        // Attack Basic
+        if (attackChoice == 0)
+        {
+            // Fire basic attack
+        }
+
+    }
+}
